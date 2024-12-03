cf1a022df6177db598ea444c0b5a951669c29001
Sophie Déziel <courrier@sophiedeziel.com>
2024-12-02 23:23:02 -0500

    refactor to initialize parts variables

--- jour-01/solution.rb
+++ jour-01/solution.rb
@@ -2,32 +2,28 @@
 class Solution < BaseSolution
     puts "jour-01"
   def initialize(input_mode)
-    list1 = []
-    list2 = []
+    @list1 = []
+    @list2 = []
 
     File.open("jour-01/#{input_mode}.txt", 'r').each do |line|
       first, last = line.split(' ').map(&:to_i)
-      list1 << first
-      list2 << last
+      @list1 << first
+      @list2 << last
     end
 
-    list1.sort!
-    list2.sort!
-
-    @total_distance = list1.zip(list2).sum do |first, last|
-      [first, last].max - [first, last].min
-    end
-
-    @similarity_score = list1.sum do |number|
-      number * list2.count(number)
-    end
+    @list1.sort!
+    @list2.sort!
   end
 
   def part1
-    @total_distance.to_s
+    @list1.zip(@list2).sum do |first, last|
+      [first, last].max - [first, last].min
+    end
   end
 
   def part2
-    @similarity_score.to_s
+    @list1.sum do |number|
+      number * @list2.count(number)
+    end
   end
 end
