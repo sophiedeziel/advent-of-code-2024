88c5eebae87a6e05bd1d5b92eec7ebaf30be2918
Sophie Déziel <courrier@sophiedeziel.com>
2024-12-03 00:40:40 -0500

    jour 3 partie 2

--- jour-03/solution.rb
+++ jour-03/solution.rb
@@ -3,9 +3,21 @@ class Solution < BaseSolution
   puts "jour-03"
   def initialize(input_mode)
     @input = File.open("jour-03/#{input_mode}.txt", 'r').read
-    muls = @input.scan(/mul\((\d+),(\d+)\)/).map { |m| m.map(&:to_i) }
-    @part1 = muls.sum { |m| m[0] * m[1] }
-    @part2 = nil
+    instructions = @input.scan(/(mul\(\d+,\d+\))|(do\(\))|(don't\(\))/).flat_map do |list|
+      list.compact
+    end
+    @part1 = sum_commands(instructions)
+
+    enabled = true
+    filtered = []
+    instructions.each do |instruction|
+      next if instruction.include?('mul(') && !enabled
+      next enabled = false if instruction.include?("don't()")
+      next enabled = true  if instruction.include?("do()")
+      filtered << instruction
+    end
+
+    @part2 = sum_commands(filtered)
   end
 
   def part1
@@ -14,5 +26,16 @@ class Solution < BaseSolution
 
   def part2
     @part2
+  end
+
+  private
+
+  def sum_commands(instructions)
+    instructions.map { |instruction| mul(instruction) if instruction.include?('mul(') }.compact.sum
+  end
+
+  def mul(string)
+    first, last = string.scan(/\d+/).map(&:to_i)
+    first * last
   end
 end
