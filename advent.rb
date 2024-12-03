5b4cee8e0b726d6e44b317b05ca8dd1d848162e1
Sophie Déziel <courrier@sophiedeziel.com>
2024-12-02 21:51:58 -0500

    jour 2 partie 2

--- advent.rb
+++ advent.rb
@@ -1,9 +1,13 @@
 require 'listen'
 require 'pry'
 
+system "clear"
+
 listener = Listen.to('.', only: /\.rb$/) do |modified, added, removed|
   modified.each do |file|
     begin
+      system "clear"
+      puts "File: #{file}"
       load(file)
     rescue => e
         puts e
--- jour-02/solution.rb
+++ jour-02/solution.rb
@@ -1,18 +1,30 @@
 
-reports = File.open('jour-02/input.txt', 'r').map do |line|
-  line.split(' ').map(&:to_i)
-end.compact
-
-changes = reports.count do |line|
-  changes = line.each_cons(2).map do |first, last|
+def safe?(*numbers)
+  changes = numbers.each_cons(2).map do |first, last|
     next if last.nil? || first.nil?
     last - first
   end
 
-  next false if changes.any? { |change| change.abs < 1 || change.abs > 3 }
-  next false unless changes.all?(&:positive?) || changes.all?(&:negative?)
-
+  return false if changes.any? { |change| change.abs < 1 || change.abs > 3 }
+  return false unless changes.all?(&:positive?) || changes.all?(&:negative?)
   true
 end
 
-puts "Partie 1: " + changes.to_s
+reports = File.open('jour-02/input.txt', 'r').map do |line|
+  line.split(' ').map(&:to_i)
+end.compact
+
+strict_safe = 0
+tolerant_safe = 0
+
+reports.each do |line|
+  if safe?(*line)
+   strict_safe += 1
+   tolerant_safe += 1
+  else
+    tolerant_safe += 1 if (0...line.count).to_a.any? { |index| safe?(*(line.reject.with_index{|v, i| i == index })) }
+  end
+end
+
+puts "Partie 1: " + strict_safe.to_s
+puts "Partie 2: " + tolerant_safe.to_s
